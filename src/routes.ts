8c03b15efc36e7f644618ab2499a49e97f1f31db
Enzo Benfica <128191773+enzobenficaa@users.noreply.github.com>
2024-03-19 10:57:35 -0300

    Add files via upload

---
+++ src/routes.ts
@@ -0,0 +1,26 @@
+import express from "express"
+import { categoriesController } from "./controllers/categoriesController"
+import { coursesController } from "./controllers/coursesController"
+import { episodesController } from "./controllers/episodesController"
+import { authController } from "./controllers/authController"
+import { ensureAuth, ensureAuthViaQuery } from "./middlewares/auth"
+import { favoritesController } from "./controllers/favoritesController"
+
+const router = express.Router()
+router.post('/auth/register',authController.register)
+router.post('/auth/login',authController.login)
+
+router.get('/categories', ensureAuth, categoriesController.index)
+router.get('/categories/:id', ensureAuth,categoriesController.show)
+
+router.get('/courses/featured', ensureAuth, coursesController.featured)
+router.get('/courses/newest', coursesController.newest)
+router.get('/courses/search', ensureAuth, coursesController.search)
+router.get('/courses/:id', ensureAuth, coursesController.show )
+
+router.get('/episodes/stream', ensureAuthViaQuery, episodesController.stream)
+
+router.post('/favorites',ensureAuth, favoritesController.save)
+
+
+export {router}
