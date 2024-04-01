4b1692d03865b4b908a1fa0e79d152e0668c7ef1
Enzo Benfica <128191773+enzobenficaa@users.noreply.github.com>
2024-04-01 11:02:46 -0300

    Add files via upload

--- src/adminJS/resources/index.ts
+++ src/adminJS/resources/index.ts
@@ -1,12 +1,30 @@
 // src/adminjs/resources/index.ts
 
 import { ResourceWithOptions } from "adminjs";
-import { Category } from "../../models";
+import { Category, Course, User } from "../../models";
 import { categoryResourceOptions } from "./category";
+import { courseResourceFeatures, courseResourceOptions } from "./course";
+import { Episode } from "../../models/Episode";
+import { episodeResourceFeatures, episodeResourceOptions } from "./episode";
+import { userResourceOptions } from "./user";
 
 export const adminJsResources: ResourceWithOptions[] = [
   {
     resource: Category,
     options: categoryResourceOptions
+  },
+  {
+    resource: Course,
+    options:courseResourceOptions,
+    features:courseResourceFeatures 
+  },
+  {
+    resource: Episode,
+    options: episodeResourceOptions,
+    features: episodeResourceFeatures
+  },
+  {
+  resource:User,
+  options:userResourceOptions
   }
 ]
